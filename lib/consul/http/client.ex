f8a2006f96b73cfbb91fb574ec3a9f8c46124e5c
Cibin George <cibin@getblueshift.com>
2017-11-02 17:13:48 -0700

    Configurable endpoint on consul. And httpoison lib upgrade

--- lib/consul/http/client.ex
+++ lib/consul/http/client.ex
@@ -1,8 +1,10 @@
 defmodule Consul.HTTP.Client do
   use HTTPoison.Base
 
+  @endpoint Application.get_env(:consul, :endpoint, "http://localhost:8500")
+
   def process_url(url) do
-    "http://localhost:8500/#{url}"
+    "#{@endpoint}/#{url}"
   end
 
   def process_response_body(body) do
